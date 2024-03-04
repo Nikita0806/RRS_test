a9be5077750c96f2b891d4ad373db3fd645aa4bf
Nikita0806 <volkov_n@garpix.com>
2024-03-04 11:49:13 +0300

    first ci push12

--- conftest.py
+++ conftest.py
@@ -1,19 +1,11 @@
-import os
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-
-@pytest.fixture(scope="function", autouse=True)          # autouse=True - она будет использоваться автоматически для всех тестов, scope="function" - создаёт браузер для каждого теста отдельно
+@pytest.fixture(scope="function",autouse=True)          # autouse=True - она будет использоваться автоматически для всех тестов, scope="function" - создаёт браузер для каждого теста отдельно
 def driver(request):                                    # Фикстура для инициализации наших тестов
     options = Options()                                 #
-    # service = Service(executable_path=ChromeDriverManager().install())          # для скачивания файлов
-    # prefs = {"download.default_directory": f"{os.getcwd()}\downloads"}          # путь к скачиванию файлов
-    # options.add_experimental_option("prefs", prefs)                             # для скачивания файлов
-    options.add_argument("--headless")                  # для безголового режима для запуска в CI
+    # options.add_argument("--headless")                  # для безголового режима для запуска в CI
     options.add_argument("--no-sandbox")                # доказательство, что это настоящий проект
     options.add_argument("--disable-dev-shm-usage")     #
     options.add_argument("--window-size=1920,1080")     # для размера окна
